9394a2e1c14b43ba7d38045ed584505bc0b26cb8
r4b61t <chayaponthunsetkul@gmail.com>
2024-01-27 23:48:07 +0700

    git ignore

--- src/new_basis_llp_qaoa/qaoa/parameters.py
+++ src/new_basis_llp_qaoa/qaoa/parameters.py
@@ -1,9 +1,21 @@
 from dataclasses import dataclass
 
 import numpy as np
+from scipy.optimize import OptimizeResult
 
 
 @dataclass
 class Parameters:
     gamma_list: np.ndarray
     beta_list: np.ndarray
+
+    @classmethod
+    def from_optimization_result(cls, res: OptimizeResult):
+        params = res.x
+        p = int(len(params)/2)
+        betas = params[:p]
+        gammas = params[p:]
+        return Parameters(
+            gamma_list=gammas,
+            beta_list=betas
+        )
--- src/new_basis_llp_qaoa/statevector_sim/statevector_llp.py
+++ src/new_basis_llp_qaoa/statevector_sim/statevector_llp.py
@@ -1,3 +1,6 @@
+from numba import njit, complex128, float64
+from numbers import Complex
+
 import math
 
 from functools import cached_property
@@ -6,6 +9,7 @@ from typing import Optional
 
 import numpy as np
 from scipy.sparse.linalg import expm
+import scipy
 from scipy.optimize import minimize
 from scipy.sparse import csc_matrix, kron, identity, dia_matrix
 from tqdm import tqdm
@@ -19,10 +23,21 @@ XNX_OP = PAULI_X @ N_OP @ PAULI_X
 PAULI_I = np.identity(2)
 
 
+def seperate_beta(beta, interval):
+    c, f = beta.__divmod__(interval)
+    c = int(c)
+    if f > interval / 2:
+        c = c + 1
+        f = f - interval
+    # if np.abs(f) < 1e-2:
+    #     f = 0
+    assert np.isclose(c * interval + f, beta, atol=2e-2), (c * interval + f, beta)
+    return c, f
+
+
 def get_energy(psi, ha) -> float:
     bra_psi = psi.conj().transpose()
     ket_psi = psi
-
     return bra_psi @ (ha @ ket_psi)
 
 
@@ -70,7 +85,12 @@ def nu_ij(i, j, max_n, loanees):
     return res
 
 
-def unitary_a(ha, gamma):
+def unitary_a(ha, gamma, use_sparse):
+    if not use_sparse:
+        dia = -1j * gamma * ha.diagonal()
+        dia = np.exp(dia)
+        return np.diag(dia)
+
     ha = dia_matrix(ha)
     shape = ha.shape
     dia = -1j * gamma * ha.diagonal()
@@ -79,18 +99,28 @@ def unitary_a(ha, gamma):
     return csc_matrix(res)
 
 
-def unitary_b(hb, beta):
-    return expm(-1j * beta * hb)
+def fast_ub(hb, beta):
+    return scipy.linalg.expm(-1j * beta * hb)
 
 
-def unitary_b_taylor(term_lists, beta):
-    def fact(i):
-        res = math.factorial(i)
-        return res
+def unitary_b(hb, beta, use_sparse):
+    if use_sparse:
+        return expm(-1j * beta * hb)
+    return fast_ub(hb.toarray(), beta)
+
+
+def unitary_b_taylor(term_lists: list[csc_matrix], beta: float, use_sparse: bool):
+    n = len(term_lists)
+    if not use_sparse:
+        term_lists = np.array([term.toarray() for term in term_lists]).T
     try:
-        return np.sum((-1j* beta) ** i * term / fact(i) for i, term in enumerate(term_lists))
+        factorials = np.array([math.factorial(i) for i in range(n)])
+        powers = (-1j * beta) ** np.arange(n)
+
+        res = powers * term_lists / factorials
+        return np.sum(res)
     except Exception as e:
-        print(beta)
+        logging.warning(beta)
         raise e
 
 
@@ -106,7 +136,10 @@ class StateVectorLLP:
         self.qubits_number = get_qubits_number(loanee_number, action_number)
         self.current_parameters = None
         self.initial_state = initial_state
+        self.use_sparse = self.qubits_number > 12
         self.psi: Optional[np.ndarray] = None
+        self.terms = 10
+        self.interval = 0.1
 
     @cached_property
     def ha(self):
@@ -114,14 +147,60 @@ class StateVectorLLP:
         return self.construct_problem_hamiltonian()
 
     @cached_property
-    def hb(self):
+    def hb(self) -> csc_matrix:
         logging.info("Caching HB.")
-        return self.construct_mixer_hamiltonian()
+        res = self.construct_mixer_hamiltonian()
+        return res
 
     @cached_property
-    def hb_taylor_terms(self, terms: int = 12) -> list:
+    def ub_integer_cache(self):
+        # logging.info("Calculating ub_small")
+        ub_small = unitary_b_taylor(self.hb_taylor_terms, self.interval, True).toarray()
+
+        res = dict()
+        ub_small_inv = np.linalg.inv(ub_small)
+        for i in tqdm(
+                range(math.ceil(1.6 // self.interval) + 1),
+                "caching small ub"
+        ):
+            if i == 0:
+                res[i] = np.identity(len(ub_small))
+            elif i == 1:
+                res[i] = ub_small
+                res[-i] = ub_small_inv
+            else:
+                res[i] = res[i - 1] @ ub_small
+                res[-i] = res[-i + 1] @ ub_small_inv
+
+        return res
+
+    def unitary_b_approx(self, beta: float, interval=0.1, terms=10):
+        """
+        cache ub with beta in [-1.6, 1.6] at 0.1 intervals
+        given beta separate it into 2 parts, beta = beta_coarse + beta_fine
+        beta_coarse, beta_fine = separate_beta(beta)
+        beta_fine = beta - beta_course
+        ub = ub(beta_course) * ub(beta_fine)
+        """
+        self.terms = terms
+        self.interval = interval
+        beta_coarse, beta_fine = seperate_beta(beta, interval)
+        # logging.warning("accessing ub coarse cache")
+        ub_integer = self.ub_integer_cache[beta_coarse]
+        # logging.warning("calcing ub_fine")
+        ub_decimal = unitary_b_taylor(self.hb_taylor_terms, beta_fine, True).toarray()
+        # logging.warning("calcing ub")
+        return ub_integer @ ub_decimal
+
+    @cached_property
+    def hb_taylor_terms(self) -> list[csc_matrix]:
+        terms = self.terms
         hb = self.hb
-        return [hb ** i for i in tqdm(range(terms), "Calculating taylor series terms for HB.")]
+        res = [identity(2 ** self.qubits_number)]
+        for i in tqdm(range(1, terms), "Calculating taylor series terms for HB."):
+            term = hb @ res[i - 1]
+            res.append(term)
+        return res
 
     def construct_problem_hamiltonian(self):
         e = self.epsilon
@@ -142,20 +221,27 @@ class StateVectorLLP:
                 extra = np.ones((self.loanee_number, 2 ** self.max_n - self.action_number)) * -99  # Create energy hills
                 h_ij = np.hstack((self.Qs, extra))[i, j]
                 res += h_ij * nu_ij(i, j, self.max_n, loanees=self.loanee_number)
-        return res
+        if self.use_sparse:
+            return res
+        return res.toarray()
 
     @property
     def welfare_term(self):
         n = self.qubits_number
         res = csc_matrix((2 ** n, 2 ** n))
+        iden = identity(2 ** n)
         for i in tqdm(range(self.loanee_number), "Calculating welfare term."):
+            nu_i0 = nu_ij(i, 0, self.max_n, loanees=self.loanee_number)
             for i_prime in range(i + 1, self.loanee_number):
-                first_factor = identity(2 ** n) - nu_ij(i, 0, self.max_n, loanees=self.loanee_number)
-                second_factor = identity(2 ** n) - nu_ij(i_prime, 0, self.max_n, loanees=self.loanee_number)
+                nu_iprime0 = nu_ij(i_prime, 0, self.max_n, loanees=self.loanee_number)
+                first_factor = iden - nu_i0
+                second_factor = iden - nu_iprime0
                 res += self.As[i, i_prime] * (first_factor @ second_factor)
-        return res
+        if self.use_sparse:
+            return res
+        return res.toarray()
 
-    def construct_mixer_hamiltonian(self):
+    def construct_mixer_hamiltonian(self) -> csc_matrix:
         n = self.qubits_number
         res = kron(identity(2 ** (n - 1)), PAULI_X)
         for q in tqdm(range(1, n), "Calculating HB."):
@@ -166,19 +252,22 @@ class StateVectorLLP:
             current_x = kron(PAULI_X, before)
             current_x = kron(after, current_x)
             res += current_x
-        return res
+        return csc_matrix(res)
 
     def evolve(self, parameters: Parameters) -> None:
         gammas = parameters.gamma_list
         betas = parameters.beta_list
-        hb_taylor_terms = self.hb_taylor_terms
         ha = self.ha
         if self.psi is None:
             self.psi = self.get_initial_state(self.qubits_number, self.initial_state)
         for gamma, beta in zip(gammas, betas):
-            ua = unitary_a(ha, gamma)
-            ub = unitary_b_taylor(hb_taylor_terms, beta)
-            self.psi = ub @ ua @ self.psi
+            ua = unitary_a(ha, gamma, self.use_sparse)
+            ub = self.unitary_b_approx(beta)
+            print(sum(self.probabilities))
+            self.psi = ua @ self.psi
+            print(sum(self.probabilities))
+            self.psi = ub @ self.psi
+            print(sum(self.probabilities))
 
     def get_energy(self) -> float:
         bra_psi = self.psi.conj().transpose()
@@ -198,12 +287,11 @@ class StateVectorLLP:
         logging.info("Evolving psi")
         self.evolve(params)
 
-    def run_optimizer(self, maxiter=None, method="COBYLA"):
+    def run_optimizer(self, maxiter=None, method="COBYLA", **kwargs):
         p = self.p
         ha = self.ha
-        hb_taylor_terms = self.hb_taylor_terms
-        beta_bounds = [(-np.pi, np.pi)]
-        gamma_bound = [(-np.pi, np.pi)]
+        beta_bounds = [(-np.pi / 2, np.pi / 2)]
+        gamma_bound = [(-np.pi / 2, np.pi / 2)]
         bounds = beta_bounds * p + gamma_bound * p
         if self.psi is None:
             self.psi = self.get_initial_state(self.qubits_number, self.initial_state)
@@ -213,13 +301,12 @@ class StateVectorLLP:
             betas = v[:p]
             gammas = v[p:]
             for gamma, beta in zip(gammas, betas):
-                if beta > 4:
-                    raise Exception
-                ua = unitary_a(ha, gamma)
-                ub = unitary_b_taylor(hb_taylor_terms, beta)
-                # ub = unitary_b(hb, beta)
+                ua = unitary_a(ha, gamma, self.use_sparse)
+                ub = self.unitary_b_approx(beta)
                 psi = ub @ ua @ psi
-            return get_energy(psi, ha).real
+            energy = get_energy(psi, ha).real
+            print(gammas,'\n', betas,'\n', energy)
+            return energy
 
         def callback(xk):
             pbar.update(1)
@@ -227,11 +314,16 @@ class StateVectorLLP:
         with tqdm(total=maxiter, desc="Optimizing") as pbar:
             optimizer_result = minimize(
                 cost,
-                np.ones(2 * p),
+                np.zeros(2 * p),
                 method=method,
-                options={'maxiter': maxiter},
+                options={
+                    'maxiter': maxiter,
+                    'tol': 1e-2,
+                    'catol': 1e-5
+                },
                 bounds=bounds,
-                callback=callback
+                callback=callback,
+                *kwargs
             )
 
         return optimizer_result
@@ -259,10 +351,9 @@ class StateVectorLLP:
         action_bit_len = int(qubits_number_for_action(self.action_number))
         return get_str(ind, q_number, action_bit_len)
 
-
     def get_cost_from_str(self, state):
         c = 0
-        h = (1-self.epsilon) * self.Qs
+        h = (1 - self.epsilon) * self.Qs
         J = self.epsilon * self.As
         for i in range(len(state)):
             action = int(state[i])
@@ -273,6 +364,7 @@ class StateVectorLLP:
                 if action == 0 and int(state[ii]) == 0:
                     c -= J[i, ii]
         return c
+
 
 def get_str(ind, q_number, action_bit_len):
     binary = bin(ind)[2:]
